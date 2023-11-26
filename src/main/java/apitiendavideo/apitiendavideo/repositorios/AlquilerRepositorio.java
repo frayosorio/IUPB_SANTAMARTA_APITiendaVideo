f7b2fb2bcff4163d2e8ff17097bf1cc0f746ad29
Fray León Osorio Rivera <frayosorio@gmail.com>
2023-11-26 10:15:20 -0500

    first commit

---
+++ src/main/java/apitiendavideo/apitiendavideo/repositorios/AlquilerRepositorio.java
@@ -0,0 +1,10 @@
+package apitiendavideo.apitiendavideo.repositorios;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import apitiendavideo.apitiendavideo.entidades.Alquiler;
+
+public interface AlquilerRepositorio extends JpaRepository<Alquiler, Long>{
+
+}
